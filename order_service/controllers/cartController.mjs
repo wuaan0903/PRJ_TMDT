81a8e9ccfd5a43ebe64f486070cb9732385091cb
wuaan0903 <nminhquan933@gmail.com>
2025-02-23 01:41:18 +0700

    add api add to cart

--- order_service/controllers/cartController.mjs
+++ order_service/controllers/cartController.mjs
@@ -1,11 +1,25 @@
 import { createCart, addToCart, getCartByUser, removeFromCart, changeQuantityandSize, deleteCart } from '../services/CartService.mjs';
 
 export const createUserCart = async (req, res) => {
+    const { userId } = req.body;
 
+    try {
+      const cart = await createCart(userId);
+      res.json(cart);
+    } catch (err) {
+      res.status(500).send('Server error');
+    }
 };
 
 export const addItemToCart = async (req, res) => {
+    const { cartId, productId, quantity, size } = req.body;
 
+    try {
+      const cart = await addToCart(cartId, productId, quantity, size);
+      res.json(cart);
+    } catch (err) {
+      res.status(500).send('Server error');
+    }
 };
 
 export const getUserCart = async (req, res) => {
--- order_service/services/CartService.mjs
+++ order_service/services/CartService.mjs
@@ -1,11 +1,37 @@
 import Cart from '../models/Cart.js';
 
 export const createCart = async (userId) => {
-
+    const cart = new Cart({
+        userId,
+        items: []
+      });
+    
+      await cart.save();
+      return cart;
 };
 
 export const addToCart = async (cartId, productId, quantity, size) => {
+    const cart = await Cart.findById(cartId);
 
+    if (!cart) {
+      throw new Error('Cart not found');
+    }
+  
+   const existingItemIndex = cart.items.findIndex(item =>
+     item.productId.toString() === productId && item.size === size
+   );
+  
+  
+   const quantityToAdd = Number(quantity); // convert quantity to number here
+  
+    if (existingItemIndex !== -1) {
+       cart.items[existingItemIndex].quantity += quantityToAdd;
+    } else {
+       cart.items.push({ productId, quantity : quantityToAdd, size });
+    }
+  
+    await cart.save();
+    return cart;
 };
 export const getCartByUser = async (userId) => {
 
